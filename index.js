c0fcd0d070759ee0a19451f9fa9b50332df112b9
Majra <majra.oruc@gmail.com>
2021-04-24 00:27:45 +0200

    Middlewares and router done, the folder structure and packages not done yet

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = require('./router')
+const PORT = 5000;
+
+const app = express();
+router(app);
+
+app.listen(PORT, error => {
+    if (error) return console.log(error);
+    console.log(`Server is listening on port ${PORT}`);
+});

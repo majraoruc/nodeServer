922364770b86d93566fcbf9cdf7d86b8d806c034
Majra <majra.oruc@gmail.com>
2021-04-24 17:21:11 +0200

    fixed nodemon for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const { node } = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: 'production',
     entry: path.join(__dirname, './src/index.js'),
     output: {
         path: path.join(__dirname, '/dist'),
